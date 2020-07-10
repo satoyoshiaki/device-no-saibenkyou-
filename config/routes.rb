0e37038b66629fb1f55472938cf60154fcd3160d
佐藤良昭 <yoshiprograming@gmail.com>
2020-07-10 23:19:17 +0900

    root

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+  root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
